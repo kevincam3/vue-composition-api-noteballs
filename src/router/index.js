f9a6e6e0c23714432cba5637e931b748c439186a
Kevin Campbell <kevin@tcmsvc.com>
2022-12-21 17:33:53 -0300

    Configure Vue Router HTML5 mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import ViewNotes from "@/views/ViewNotes.vue";
 import ViewEditNote from "@/views/ViewEditNote.vue";
 import ViewStats from "@/views/ViewStats.vue";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
